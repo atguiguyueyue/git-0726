31973df9cd89d7df9c2193110d25835c50bdf8f6
SH0726 <SH0726@atguigu.com>
2021-11-29 15:41:38 +0800

    hot-fix commit

--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello world");
+        System.out.println("hot-fix");
     }
 }
